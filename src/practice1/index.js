ec979fa6a789ff9de4fb1482a8bf1665793780eb
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-12 23:58:35 +0800

    completed practice1 practice2 practice3

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,8 @@
+import { ADDRESS, PATH, PORT, REGION } from "./constant";
+import getURL from "./utils";
 
+const url = getURL(ADDRESS, PORT, PATH);
+
+const region = REGION;
+
+export { url, region };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,15 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const result = [];
+      result.push(data.origin);
+      result.push(data.author);
+      result.push(data.content);
+      return result;
+    })
+    .catch((err) => console.log("rejected: ", err));
+};
